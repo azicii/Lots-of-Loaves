8d27bf1b2d47ecc593b67f0cb765200174041b69
Aziz Hamza <azizhamza0@gmail.com>
2022-11-12 23:14:39 -0800

    created main menu canvas with functionality for play button
    Created functionality for menu system and allow for player to use play button. other buttons have no functionality yet because there are no canvases available for them. will work on creating options, credit, and pause menu canvases later on.

---
+++ Assets/Scripts/MenuTracker.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class MenuTracker : MonoBehaviour
+{
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ReloadLevel()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+        this.gameObject.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        if (level == 0)
+        {
+            Time.timeScale = 1;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
